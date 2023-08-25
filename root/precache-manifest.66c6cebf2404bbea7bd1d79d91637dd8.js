e16c56c597238bf0fa9b79d0ab27e92362926775
ArchanaOjha3 <ojhaabhishekkumar@gmail.com>
2023-08-25 18:15:23 +0530

    build:created root folder for gihub pages

---
+++ root/precache-manifest.66c6cebf2404bbea7bd1d79d91637dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29cf267941147d414054",
+    "url": "/static/css/main.ec918811.chunk.css"
+  },
+  {
+    "revision": "29cf267941147d414054",
+    "url": "/static/js/main.29cf2679.chunk.js"
+  },
+  {
+    "revision": "4e4f9d7e6bec88624d36",
+    "url": "/static/css/1.f7e65dd3.chunk.css"
+  },
+  {
+    "revision": "4e4f9d7e6bec88624d36",
+    "url": "/static/js/1.4e4f9d7e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "244b72f7052a773b8634dc94843b412f",
+    "url": "/index.html"
+  }
+];
